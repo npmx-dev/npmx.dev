d01037ad2495ec3fb6eb45b42f01d38994b5bc03
Bailey Townsend <baileytownsend2323@gmail.com>
2026-01-30 18:03:14 -0600

    bit more explicit

--- server/api/auth/session.get.ts
+++ server/api/auth/session.get.ts
@@ -1,5 +1,11 @@
-import type { UserSession } from '#shared/schemas/userSession'
+import { UserSessionSchema } from '#shared/schemas/userSession'
+import { safeParse } from 'valibot'
 
 export default eventHandlerWithOAuthSession(async (event, oAuthSession, serverSession) => {
-  return serverSession.data as UserSession
+  const result = safeParse(UserSessionSchema, serverSession.data)
+  if (!result.success) {
+    return null
+  }
+
+  return result.output
 })
