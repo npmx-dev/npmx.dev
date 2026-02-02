94240a9d5c1f616d62ade1936d7e462f9e3c75ed
Marcus Blättermann <marcus@essenmitsosse.de>
2026-02-02 14:24:00 +0100

    style: remove hover from unclickable tags

--- app/components/Tag/Static.vue
+++ app/components/Tag/Static.vue
@@ -5,7 +5,7 @@ const props = withDefaults(defineProps<{ as?: string | Component }>(),{ as: 'spa
 <template>
   <component
     :is="props.as"
-    class="inline-flex items-center px-2 py-0.5 text-xs font-mono text-fg-muted bg-bg-muted border border-border rounded transition-colors duration-200 hover:(text-fg border-border-hover)"
+    class="inline-flex items-center px-2 py-0.5 text-xs font-mono text-fg-muted bg-bg-muted border border-border rounded"
   >
     <slot />
   </component>
