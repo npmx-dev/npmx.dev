cf816efdfc7cf6d08c55e849d671212d58432a99
Philippe Serhal <philippe.serhal@gmail.com>
2026-02-02 22:30:17 -0500

    Revert "chore: add debug logs and try fixing GitHub Actions detection"
    This reverts commit d50623b1a46e92257b3c90a1a71b2dc78faa079c.

--- modules/production.ts
+++ modules/production.ts
@@ -9,19 +9,7 @@ export default defineNuxtModule({
   setup() {
     const nuxt = useNuxt()
 
-    const isGitHubActions = provider === 'github_actions' || !!process.env.GITHUB_ACTIONS
-    const isTest = process.env.NODE_ENV === 'test'
-
-    console.log('[prod-env]', {
-      isCI,
-      provider,
-      isGitHubActions,
-      isTest,
-      GITHUB_ACTIONS: process.env.GITHUB_ACTIONS,
-    })
-
-    if (isCI && !isGitHubActions && !isTest) {
-      console.log('[prod-env] Enabling hydration debugging')
+    if (isCI && provider !== 'github_actions') {
       nuxt.options.debug = defu(nuxt.options.debug, { hydration: true })
     }
   },
