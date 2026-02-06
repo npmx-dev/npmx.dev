3a13fbfa8b799e6a0193bf432173d86c07ab55a6
Marcus Blättermann <marcus@essenmitsosse.de>
2026-02-06 21:12:16 +0100

    docs: rephrase comment

--- app/components/Button/Base.vue
+++ app/components/Button/Base.vue
@@ -8,7 +8,7 @@ const props = withDefaults(
     'keyshortcut'?: string
 
     /**
-     * Don't use this directly, use `keyshortcut` instead. Correctly HTML will be automatically generated and the shortcut will automatically be displayed in the UI.
+     * Do not use this directly. Use keyshortcut instead; it generates the correct HTML and displays the shortcut in the UI.
      */
     'aria-keyshortcuts'?: never
 
