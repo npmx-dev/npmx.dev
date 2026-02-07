09c99c1c13a267143143fe8bad454c0dae5c8923
Bailey Townsend <baileytownsend2323@gmail.com>
2026-02-06 20:54:21 -0600

    Like endpoint

---
+++ server/api/social/profile/[identifier]/index.get.ts
@@ -0,0 +1,14 @@
+export default defineEventHandler(async event => {
+  const identifier = getRouterParam(event, 'identifier')
+  if (!identifier) {
+    throw createError({
+      status: 400,
+      message: 'identifier not provided',
+    })
+  }
+
+  const profileUtil = new ProfileUtils()
+  const profile = await profileUtil.getProfile(identifier)
+  console.log('ENDPOINT', { identifier, profile })
+  return profile
+})
