69bc16ab739c3eec49bbb9ee41abaec23b8c8c13
Roman <dev@rman.dev>
2026-02-10 09:49:28 +0000

    fix

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 import { SHOWCASED_FRAMEWORKS } from '~/utils/frameworks'
 
-const { isAlgolia } = useSearchProvider()
-
 const searchQuery = shallowRef('')
 const isSearchFocused = shallowRef(false)
 
