7b1d06331378aba8d59d2c6fec107da1b99ae1da
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2026-02-12 19:12:26 +0530

    chore: reduce reload time to 1 second

--- server/plugins/fallback-ui.ts
+++ server/plugins/fallback-ui.ts
@@ -1,7 +1,7 @@
 export default defineNitroPlugin(nitroApp => {
   nitroApp.hooks.hook('render:html', (html, { event }) => {
     if (event.path.includes('/200.html')) {
-      html.head.push('<noscript><meta http-equiv="refresh" content="3"></noscript>')
+      html.head.push('<noscript><meta http-equiv="refresh" content="1"></noscript>')
     }
   })
 })
