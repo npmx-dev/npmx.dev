145cd00c2f4dcd4905af0473fa1423aef0b609c6
WilcoSp <wilcosp@hotmail.nl>
2026-03-06 21:37:34 +0100

    moved from switch case to `te` function to check whether view_on.{provider}

--- app/composables/useViewOnGitProvider.ts
+++ app/composables/useViewOnGitProvider.ts
@@ -4,31 +4,12 @@
 export function useViewOnGitProvider(
   provider: MaybeRefOrGetter<ProviderId | (string & {}) | null | undefined>,
 ) {
-  const { t } = useI18n()
+  const { t, te } = useI18n()
   return computed(() => {
     const uProvider = toValue(provider)
-    // using this switch instead of `view_on.${provider}` to prevent missing future translations
-    switch (uProvider) {
-      case 'github':
-        return t('common.view_on.github')
-      case 'gitlab':
-        return t('common.view_on.gitlab')
-      case 'bitbucket':
-        return t('common.view_on.bitbucket')
-      case 'codeberg':
-        return t('common.view_on.codeberg')
-      case 'forgejo':
-        return t('common.view_on.forgejo')
-      case 'gitea':
-        return t('common.view_on.gitea')
-      case 'gitee':
-        return t('common.view_on.gitee')
-      case 'radicle':
-        return t('common.view_on.radicle')
-      case 'sourcehut':
-        return t('common.view_on.sourcehut')
-      case 'tangled':
-        return t('common.view_on.tangled')
+    const key = `common.view_on.${uProvider}`
+    if (uProvider && te(key)) {
+      return t(key)
     }
     return t('common.view_on.git_repo')
   })
