5d19137e31f043eee166b545cf64196d0f23bdb5
WilcoSp <wilcosp@hotmail.nl>
2026-03-07 10:36:50 +0100

    the view_on translations for the git providers are now in the comments to satisfy i18n report

--- app/composables/useViewOnGitProvider.ts
+++ app/composables/useViewOnGitProvider.ts
@@ -4,32 +4,31 @@
 export function useViewOnGitProvider(
   provider: MaybeRefOrGetter<ProviderId | (string & {}) | null | undefined>,
 ) {
-  const { t } = useI18n()
+  const { t, te } = useI18n()
   return computed(() => {
     const uProvider = toValue(provider)
-    // using this switch instead of `view_on.${provider}` to prevent missing future translations
-    switch (uProvider) {
-      case 'github':
-        return t('common.view_on.github')
-      case 'gitlab':
-        return t('common.view_on.gitlab')
-      case 'bitbucket':
-        return t('common.view_on.bitbucket')
-      case 'codeberg':
-        return t('common.view_on.codeberg')
-      case 'forgejo':
-        return t('common.view_on.forgejo')
-      case 'gitea':
-        return t('common.view_on.gitea')
-      case 'gitee':
-        return t('common.view_on.gitee')
-      case 'radicle':
-        return t('common.view_on.radicle')
-      case 'sourcehut':
-        return t('common.view_on.sourcehut')
-      case 'tangled':
-        return t('common.view_on.tangled')
+    if (!uProvider) {
+      return t('common.view_on.git_repo')
     }
+    const key = `common.view_on.${uProvider}`
+    if (te(key)) {
+      return t(key)
+    }
+    // oxlint-disable-next-line no-console
+    console.warn(`"common.view_on.${uProvider}" translation doesn't exist`)
     return t('common.view_on.git_repo')
   })
 }
+
+/* for i18n report, translations that are currently present:
+t('common.view_on.github') 
+t('common.view_on.gitlab') 
+t('common.view_on.bitbucket') 
+t('common.view_on.codeberg')
+t('common.view_on.forgejo')
+t('common.view_on.gitea')
+t('common.view_on.gitee')
+t('common.view_on.radicle')
+t('common.view_on.sourcehut')
+t('common.view_on.tangled')
+ */
