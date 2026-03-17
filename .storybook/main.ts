d45844a07cfa69448930be72def2f60d365dc387
cylewaitforit <cyle@cylewaitfor.it>
2026-03-17 17:07:51 -0500

    docs(ui): add storybook welcome page

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook-vue/nuxt'
 
 const config = {
-  stories: ['../app/**/*.stories.@(js|ts)'],
+  stories: ['../.storybook/*.mdx', '../app/**/*.stories.@(js|ts)'],
   addons: ['@storybook/addon-a11y', '@storybook/addon-docs', '@storybook/addon-themes'],
   framework: '@storybook-vue/nuxt',
   staticDirs: ['./.public'],
