0bd8a72229f3a2d99c58cd960c56c42a26b5af1c
cylewaitforit <cyle@cylewaitfor.it>
2026-03-23 18:54:07 -0500

    docs(ui): add design system guidelines
    Co-authored-by: Alex Savelyev <91429106+alexdln@users.noreply.github.com>

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook-vue/nuxt'
 
 const config = {
-  stories: ['../.storybook/*.mdx', '../app/**/*.stories.@(js|ts)'],
+  stories: ['../.storybook/*.mdx', '../app/**/*.@(mdx|stories.@(js|ts))'],
   addons: [
     '@storybook/addon-a11y',
     '@storybook/addon-docs',
