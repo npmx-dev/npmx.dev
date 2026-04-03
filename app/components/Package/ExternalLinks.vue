6eec73676feef923adf6b45324c87e7c820e3710
Roman <dev@rman.dev>
2026-04-03 13:09:19 +0100

    feat: add link to `socket.dev`

--- app/components/Package/ExternalLinks.vue
+++ app/components/Package/ExternalLinks.vue
@@ -175,6 +175,14 @@ useCommandPaletteContextCommands(
     </li>
     <li>
       <LinkBase
+        :to="`https://socket.dev/npm/package/${pkg.name}`"
+        :title="$t('common.view_on.socket_dev')"
+      >
+        socket.dev
+      </LinkBase>
+    </li>
+    <li>
+      <LinkBase
         :to="`https://www.npmjs.com/package/${pkg.name}`"
         :title="$t('common.view_on.npm')"
         classicon="i-simple-icons:npm"
