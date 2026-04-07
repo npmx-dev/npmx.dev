edfd26c44b5611bfc44bc65206d5565dd676ccc1
Roman <dev@rman.dev>
2026-04-07 12:12:26 +0100

    fix

--- app/utils/module-replacements.ts
+++ app/utils/module-replacements.ts
@@ -1,9 +1,11 @@
-export function getReplacementDescription(replacement: ModuleReplacement) {
+import type { ModuleReplacement } from 'module-replacements'
+
+export function getReplacementDescription(replacement: ModuleReplacement): string {
   if (replacement.type === 'documented') return ''
   return replacement.description ?? ''
 }
 
-export function getReplacementNodeVersion(replacement: ModuleReplacement) {
+export function getReplacementNodeVersion(replacement: ModuleReplacement): string | null {
   const nodeEngine = replacement.engines?.find(e => e.engine === 'nodejs')
   return nodeEngine?.minVersion || null
 }
