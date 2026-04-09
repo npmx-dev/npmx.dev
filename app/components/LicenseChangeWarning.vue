c3b5986260f126ab5471195f2845b4b9aa2c0dcd
Willow (GHOST) <git@willow.sh>
2026-04-09 12:53:04 -0400

    chore: fmt

--- app/components/LicenseChangeWarning.vue
+++ app/components/LicenseChangeWarning.vue
@@ -11,6 +11,7 @@ const { data: licenseData } = useLicenseChanges(
   () => props.packageName,
   () => props.resolvedVersion,
 )
+
 const licenseChangeRecord = computed(() => licenseData?.value?.change ?? null)
 </script>
 
