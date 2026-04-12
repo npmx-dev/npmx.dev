e74c83beac7443003a07612dbf1d5513a57a2352
Raman Paulau <rspaulau@gmail.com>
2026-04-12 14:48:05 -0700

    feat: add npmjs.dev to playground links (#2314)

--- app/pages/package/[[org]]/[name].vue
+++ app/pages/package/[[org]]/[name].vue
@@ -63,20 +63,33 @@ const { data: readmeData } = useLazyFetch<ReadmeResponse>(
   },
 )
 
-const playgroundLinks = computed(() => [
-  ...readmeData.value.playgroundLinks,
+const playgroundLinks = computed(() => {
+  const links = [...readmeData.value.playgroundLinks]
+
+  const packageData = pkg.value
+  if (!packageData) {
+    return links
+  }
+
   // Libraries with a storybook field in package.json contain a link to their deployed playground
-  ...(pkg.value?.storybook?.url
-    ? [
-        {
-          url: pkg.value.storybook.url,
-          provider: 'storybook',
-          providerName: 'Storybook',
-          label: 'Storybook',
-        },
-      ]
-    : []),
-])
+  if (packageData.storybook?.url) {
+    links.push({
+      url: packageData.storybook.url,
+      provider: 'storybook',
+      providerName: 'Storybook',
+      label: 'Storybook',
+    })
+  }
+
+  links.push({
+    url: `https://npmjs.dev/${packageData.name}`,
+    provider: 'npmjs.dev',
+    providerName: 'npmjs.dev',
+    label: 'npmjs.dev',
+  })
+
+  return links
+})
 
 const {
   data: readmeMarkdownData,
