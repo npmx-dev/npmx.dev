38de7780ded5f28d93c512ebc2b9a66988015111
graphieros <alec.lloyd.probert@gmail.com>
2026-05-16 23:18:53 +0200

    fix: increase right padding

--- app/components/Package/TrendsChart.vue
+++ app/components/Package/TrendsChart.vue
@@ -1392,7 +1392,7 @@ const chartConfig = computed<VueUiXyConfig>(() => {
     chart: {
       height: chartHeight.value,
       backgroundColor: colors.value.bg,
-      padding: { bottom: displayedGranularity.value === 'yearly' ? 84 : 64, right: 128 }, // padding right is set to leave space of last datapoint label(s)
+      padding: { bottom: displayedGranularity.value === 'yearly' ? 84 : 64, right: 145 }, // padding right is set to leave space of last datapoint label(s)
       userOptions: {
         buttons: {
           pdf: false,
