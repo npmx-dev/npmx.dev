020cde68ff77fcd38e7df5bd50a1df8255775365
Vordgi <sasha2822222@gmail.com>
2026-05-21 21:37:23 +0100

    feat: add nodejs noodle

---
+++ app/components/Noodle/Nodejs/Logo.vue
@@ -0,0 +1,90 @@
+<template>
+  <TooltipApp interactive position="top" :tooltipAttr="{ class: 'w-[calc(100%-2rem)]! max-w-sm!' }">
+    <template #content>
+      <p class="text-sm font-medium text-fg mb-1">
+        <span class="text-fg-subtle">
+          // 27 May 2009 - nodejs Initial release
+          <br />
+          //&nbsp;<a
+            href="https://github.com/nodejs/node-v0.x-archive/releases/tag/v0.0.1"
+            target="_blank"
+            class="underline"
+            >github.com/nodejs/node-v0.x-archive/releases/tag/v0.0.1</a
+          >
+        </span>
+        <br />
+        <span class="text-badge-yellow">console</span>.<span class="text-badge-blue">log(</span
+        ><span class="text-badge-green">"Happy birthday, nodejs"</span
+        ><span class="text-badge-blue">)</span>
+      </p>
+    </template>
+    <svg
+      width="996"
+      height="305"
+      viewBox="0 0 996 305"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      :alt="$t('alt_logo')"
+      class="mb-8 mx-auto w-72 h-auto sm:w-92 max-w-full"
+    >
+      <path
+        d="M304.5 212.5V100.5L364.5 60.5L424.5 100.5V212.5L388.5 188.5V120.5L364.5 104.5L340.5 120.5V188.5L304.5 212.5Z"
+        fill="currentColor"
+        stroke="currentColor"
+        stroke-width="5"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M904.5 184.5L868.5 208.5V172.5L957.5 84.5L993.5 60.5V96.5L904.5 184.5Z"
+        fill="currentColor"
+        stroke="currentColor"
+        stroke-width="5"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M957.5 184.5L993.5 208.5V172.5L904.5 84.5L868.5 60.5V96.5L957.5 184.5Z"
+        fill="currentColor"
+        stroke="currentColor"
+        stroke-width="5"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M624.5 212.5V100.5L684.5 60.5L726.5 88.5L768.5 60.5L828.5 100.5V188.5L792.5 212.5V120.5L768.5 104.5L744.5 120.5V188.5L708.5 212.5V188.5V120.5L684.5 104.5L660.5 120.5V188.5L624.5 212.5Z"
+        fill="currentColor"
+      />
+      <path
+        d="M744.5 188.5H708.5V120.5L684.5 104.5L660.5 120.5V188.5L624.5 212.5V100.5L684.5 60.5L744.5 100.5V188.5ZM744.5 188.5L708.5 212.5V100.5L768.5 60.5L828.5 100.5V188.5L792.5 212.5V120.5L768.5 104.5L744.5 120.5V188.5Z"
+        stroke="currentColor"
+        stroke-width="5"
+        stroke-linejoin="round"
+      />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M464.5 100.5V164.5V302.5L500.5 278.5L500.5 188.5L524.5 204.5L548.5 188.5L584.5 164.5V100.5L524.5 60.5L464.5 100.5ZM548.5 144.5V120.5L524.5 104.5L500.5 120.5L500.5 144.5L524.5 160.5L548.5 144.5Z"
+        fill="currentColor"
+        stroke="currentColor"
+        stroke-width="5"
+        stroke-linejoin="round"
+      />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M50.5 172.5V196.5L26.5 212.5L2.5 196.5L2.5 172.5L26.5 156.5L50.5 172.5Z"
+        fill="#689F63"
+        stroke="#689F63"
+        stroke-width="5"
+        stroke-linejoin="round"
+      />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M164.501 26.5L102.5 212.5L146.5 188.5L208.5 2.50003L164.501 26.5Z"
+        fill="#689F63"
+        stroke="#689F63"
+        stroke-width="5"
+        stroke-linejoin="round"
+      />
+    </svg>
+  </TooltipApp>
+</template>
