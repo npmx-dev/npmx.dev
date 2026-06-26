d55cbb44c8109c55eeab2e9b812fa953c7fb4bee
Vordgi <sasha2822222@gmail.com>
2026-06-26 19:58:14 +0100

    feat: split sponsors to diff levels

--- app/assets/logos/sponsors/index.ts
+++ app/assets/logos/sponsors/index.ts
@@ -20,68 +20,72 @@ import LogoChromaticLight from './chromatic-light.svg'
 // If there are no original assets and the logo is not universal, you can add only the dark theme variant
 // and specify 'auto' for the light one - this will grayscale the logo and invert it in light mode.
 // The normalisingIndent is the Y-axis space to visually stabilize favicon-only logos with logotypes that contain long name.
-export const SPONSORS = [
-  {
-    name: 'Vercel',
-    logo: {
-      dark: LogoVercel,
-      light: LogoVercelLight,
+export const SPONSORS = {
+  gold: [
+    {
+      name: 'Vercel',
+      logo: {
+        dark: LogoVercel,
+        light: LogoVercelLight,
+      },
+      normalisingIndent: '0.875rem',
+      url: 'https://vercel.com/',
     },
-    normalisingIndent: '0.875rem',
-    url: 'https://vercel.com/',
-  },
-  {
-    name: 'Void Zero',
-    logo: {
-      dark: LogoVoidZero,
-      light: LogoVoidZeroLight,
+  ],
+  silver: [
+    {
+      name: 'Void Zero',
+      logo: {
+        dark: LogoVoidZero,
+        light: LogoVoidZeroLight,
+      },
+      normalisingIndent: '0.875rem',
+      url: 'https://voidzero.dev/',
     },
-    normalisingIndent: '0.875rem',
-    url: 'https://voidzero.dev/',
-  },
-  {
-    name: 'vlt',
-    logo: {
-      dark: LogoVlt,
-      light: LogoVltLight,
+    {
+      name: 'vlt',
+      logo: {
+        dark: LogoVlt,
+        light: LogoVltLight,
+      },
+      normalisingIndent: '0.875rem',
+      url: 'https://vlt.sh/',
     },
-    normalisingIndent: '0.875rem',
-    url: 'https://vlt.sh/',
-  },
-  {
-    name: 'Netlify',
-    logo: {
-      dark: LogoNetlify,
-      light: LogoNetlifyLight,
+    {
+      name: 'Netlify',
+      logo: {
+        dark: LogoNetlify,
+        light: LogoNetlifyLight,
+      },
+      normalisingIndent: '0.25rem',
+      url: 'https://netlify.com/',
     },
-    normalisingIndent: '0.25rem',
-    url: 'https://netlify.com/',
-  },
-  {
-    name: 'Bluesky',
-    logo: {
-      dark: LogoBluesky,
-      light: LogoBlueskyLight,
+    {
+      name: 'Bluesky',
+      logo: {
+        dark: LogoBluesky,
+        light: LogoBlueskyLight,
+      },
+      normalisingIndent: '0.625rem',
+      url: 'https://bsky.app/',
     },
-    normalisingIndent: '0.625rem',
-    url: 'https://bsky.app/',
-  },
-  {
-    name: 'Chromatic',
-    logo: {
-      dark: LogoChromatic,
-      light: LogoChromaticLight,
+    {
+      name: 'Chromatic',
+      logo: {
+        dark: LogoChromatic,
+        light: LogoChromaticLight,
+      },
+      normalisingIndent: '0.5rem',
+      url: 'https://chromatic.com/',
     },
-    normalisingIndent: '0.5rem',
-    url: 'https://chromatic.com/',
-  },
-  {
-    name: 'Badrap',
-    logo: {
-      dark: LogoBadrap,
-      light: LogoBadrapLight,
+    {
+      name: 'Badrap',
+      logo: {
+        dark: LogoBadrap,
+        light: LogoBadrapLight,
+      },
+      normalisingIndent: '0.5rem',
+      url: 'https://badrap.io/',
     },
-    normalisingIndent: '0.5rem',
-    url: 'https://badrap.io/',
-  },
-]
+  ],
+}
