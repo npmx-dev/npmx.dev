d59d3b6f324f60068798086f728fc8b2af7da0ad
Willow (GHOST) <git@willow.sh>
2026-07-06 00:58:49 +0100

    chore: remove hack

--- vite.config.ts
+++ vite.config.ts
@@ -218,10 +218,6 @@ export default defineConfig({
               provider: playwright(),
               instances: [{ browser: 'chromium', headless: true }],
             },
-            alias: {
-              // Hack for 'Error: Missing "#oauth/config" specifier in "nuxt" package' error
-              '#oauth/config': '',
-            },
           },
         }),
     ],
