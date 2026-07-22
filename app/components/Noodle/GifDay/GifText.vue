ce87c272176dd521278d29840fc229bb59b885a1
MatteoGabriele <m.gabriele.dev@gmail.com>
2026-07-22 21:17:20 +0200

    feat: add select-none

--- app/components/Noodle/GifDay/GifText.vue
+++ app/components/Noodle/GifDay/GifText.vue
@@ -15,7 +15,7 @@ const cssUrl = computed<string>(() => `url(${props.backgroundUrl})`)
     :title
     :class="[
       'gif-text-image',
-      'font-noodle leading-none bg-cover bg-center motion-safe:text-transparent bg-clip-text text-[4rem] @xl:text-[14rem] font-extrabold',
+      'font-noodle select-none leading-none bg-cover bg-center motion-safe:text-transparent bg-clip-text text-[4rem] @xl:text-[14rem] font-extrabold',
     ]"
     :style="{ backgroundPosition, backgroundSize }"
   >
