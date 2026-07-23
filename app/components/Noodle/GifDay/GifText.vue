043b933933ec309fe9faec7f071120a924c769bc
MatteoGabriele <m.gabriele.dev@gmail.com>
2026-07-23 19:12:43 +0200

    fix: hydration mismatch

--- app/components/Noodle/GifDay/GifText.vue
+++ app/components/Noodle/GifDay/GifText.vue
@@ -7,24 +7,21 @@ const props = defineProps<{
   backgroundSize?: string
 }>()
 
-const cssUrl = computed<string>(() => `url(${props.backgroundUrl})`)
+const style = computed(() => ({
+  backgroundImage: `url(${props.backgroundUrl})`,
+  backgroundPosition: props.backgroundPosition,
+  backgroundSize: props.backgroundSize,
+}))
 </script>
 
 <template>
   <span
     :title
+    :style
     :class="[
-      'gif-text-image',
       'font-noodle select-none leading-none bg-cover bg-center motion-safe:text-transparent bg-clip-text text-[4rem] @xl:text-[14rem] font-extrabold',
     ]"
-    :style="{ backgroundPosition, backgroundSize }"
   >
     {{ text }}
   </span>
 </template>
-
-<style scoped>
-.gif-text-image {
-  background-image: v-bind(cssUrl);
-}
-</style>
