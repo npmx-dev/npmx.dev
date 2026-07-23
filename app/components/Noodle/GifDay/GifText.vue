b40b9727ad60eeb944db571ef1b41618a9c9fba1
MatteoGabriele <m.gabriele.dev@gmail.com>
2026-07-23 20:52:39 +0200

    feat: add preloader

--- app/components/Noodle/GifDay/GifText.vue
+++ app/components/Noodle/GifDay/GifText.vue
@@ -7,8 +7,18 @@ const props = defineProps<{
   backgroundSize?: string
 }>()
 
+const loaded = ref(false)
+
+onMounted(() => {
+  const image = new Image()
+  image.addEventListener('load', () => {
+    loaded.value = true
+  })
+  image.src = props.backgroundUrl
+})
+
 const style = computed(() => ({
-  backgroundImage: `url(${props.backgroundUrl})`,
+  backgroundImage: loaded.value ? `url(${props.backgroundUrl})` : undefined,
   backgroundPosition: props.backgroundPosition,
   backgroundSize: props.backgroundSize,
 }))
@@ -19,7 +29,8 @@ const style = computed(() => ({
     :title
     :style
     :class="[
-      'font-noodle select-none leading-none bg-cover bg-center motion-safe:text-transparent bg-clip-text text-[4rem] @xl:text-[14rem] font-extrabold',
+      'font-noodle select-none leading-none bg-cover bg-center motion-safe:text-transparent bg-clip-text text-[4rem] @xl:text-[14rem] font-extrabold transition-[background-image] duration-300',
+      !loaded && 'bg-fg-muted/20 animate-pulse',
     ]"
   >
     {{ text }}
